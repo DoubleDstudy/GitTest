5b2cf8201cf377510aedecb99638adf9712cc7aa
yusungmin <butter647@hanmail.net>
2019-12-22 00:14:50 +0900

    2

---
+++ src/B.java
@@ -0,0 +1,19 @@
+public class B {
+
+    public void ten(){
+        System.out.println("11124");
+        System.out.println("1234123");
+        System.out.println("11111");
+        System.out.println("g2222");
+        System.out.println("good");
+        System.out.println("good2");
+        System.out.println("great");
+        System.out.println("good");
+        System.out.println("good");
+        System.out.println("good");
+        System.out.println("good2");
+    }
+
+
+
+}
