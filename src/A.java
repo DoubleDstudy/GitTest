794c41c5e2897b047a9339b36db2133a96eabad4
yusungmin <butter647@hanmail.net>
2019-12-22 00:09:50 +0900

    git test

---
+++ src/A.java
@@ -0,0 +1,15 @@
+public class A {
+
+    public void ten(){
+        System.out.println("good");
+        System.out.println("good");
+        System.out.println("good");
+        System.out.println("good");
+        System.out.println("great");
+        System.out.println("good");
+        System.out.println("good");
+        System.out.println("good");
+        System.out.println("good2");
+    }
+
+}
